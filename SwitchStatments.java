0562f2064c8e8e6b14c78cade756deb875fc5f9d
Mike Meadows <mmeadow33@gmail.com>
2017-01-16 16:28:57 -0600

    switch statements

---
+++ SwitchStatments.java
@@ -0,0 +1,19 @@
+
+public class SwitchStatments {
+	public static void main(String [] args){
+		int age;
+		age = 26;
+		
+		switch (age){
+		case 1: 
+			System.out.println("not the same age");
+			break;
+		case 25: 
+			System.out.println("it is  the same age");
+			break;
+		default:  	
+			System.out.println("This is the default value");
+			break;
+		}
+	}
+}
