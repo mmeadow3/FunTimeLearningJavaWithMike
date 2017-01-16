b8e4849c615ca2c0a8913ea3c1cea79573806578
Mike Meadows <mmeadow33@gmail.com>
2017-01-16 14:14:23 -0600

    basic calculator examples

---
+++ $javaCalc.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+/////importing this allows to gather user input
+public class $javaCalc {
+	public static void main(String[] args){
+		///make variabe to hold user input
+		////System in is System input
+		Scanner userInput = new Scanner(System.in);
+		double num1, num2, answer;
+		System.out.println("Enter a number");
+		//////nextDouble is a provided method. could be nextString()
+		num1 = userInput.nextDouble();
+		System.out.println("Enter another number");
+		num2 = userInput.nextDouble();
+		System.out.println("The sume of your numbers is " + (num1 + num2));
+	}
+}
+
+//output
+//Enter a number
+//7.7
+//Enter another number
+//8.8
+//The sume of your numbers is 16.5
