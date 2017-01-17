adc724e27287bd53a1a7238b09b61fd36f25849c
Mike Meadows <mmeadow33@gmail.com>
2017-01-16 21:43:01 -0600

    taking arguments from multiple files

--- MultipleClasses.java
+++ MultipleClasses.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class MultipleClasses {
 	public static void main(String [] args){
 		//////this is the one that contains main()
+		Scanner input = new Scanner(System.in);
+		
 		MultipleClasses1 MutlitpleClassObject = new MultipleClasses1();
-		MutlitpleClassObject.print();
+		
+		
+		System.out.println("Please enter your name");
+		/////make a variable to hold name data
+		String name =  input.nextLine();
+		//////this time pass the parameter
+		MutlitpleClassObject.print(name);
 	}
 }
--- MultipleClasses1.java
+++ MultipleClasses1.java
@@ -1,6 +1,8 @@
 
 public class MultipleClasses1 {
-	public void print(){
-		System.out.println("This is the second class");
+	public void print(String name){
+//		System.out.println("This is the second class");
+		System.out.println("Hello " + name);
+
 	}
 }
