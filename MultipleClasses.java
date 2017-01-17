e1bf7fc062f8df9faac13b975f2995d4ef4c95c9
Mike Meadows <mmeadow33@gmail.com>
2017-01-16 21:13:00 -0600

    demo multiple classes

---
+++ MultipleClasses.java
@@ -0,0 +1,8 @@
+
+public class MultipleClasses {
+	public static void main(String [] args){
+		//////this is the one that contains main()
+		MultipleClasses1 MutlitpleClassObject = new MultipleClasses1();
+		MutlitpleClassObject.print();
+	}
+}
---
+++ MultipleClasses1.java
@@ -0,0 +1,6 @@
+
+public class MultipleClasses1 {
+	public void print(){
+		System.out.println("This is the second class");
+	}
+}
